9349a09afaf9e8dcaeac090a0bbfd830f542bc20
Julian Duque Tabares <julianduque85@gmail.com>
2023-02-27 19:03:07 -0500

    Initial commit

---
+++ POS.Infrastructure/Persistences/Contexts/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A0BC86EB6A3");
+
+            builder.Property(e => e.Name).HasMaxLength(100);
+        }
+    }
+}
