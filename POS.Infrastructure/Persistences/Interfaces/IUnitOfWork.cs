c2456ff148de70ff0d8be8c7f544417c2714a1f5
Julian Duque Tabares <julianduque85@gmail.com>
2023-05-24 09:09:05 -0500

    Unit of work pattern

---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaración o matricula de nuestra interface a nivel repositorio
+        ICategoryRepository Category { get; }
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
